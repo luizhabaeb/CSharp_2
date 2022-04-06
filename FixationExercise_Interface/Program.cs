7f51fe5475bf6ce4bc5272ae71f4a9dfb1907968
Luiz Habaeb <ELISILU@ericsson.com>
2022-04-06 20:24:18 -0300

    Icomparable test

---
+++ FixationExercise_Interface/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using FixationExercise_Interface.Entities;
+using FixationExercise_Interface.Services;
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("Enter contract data: ");
+Console.ResetColor();
+Console.Write("Number: ");
+int contractNumber = int.Parse(Console.ReadLine());
+Console.Write("Date (dd/MM/yyy): ");
+DateTime contractDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+Console.Write("Contract value: ");
+double contractValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Enter the number of installments: ");
+int months = int.Parse(Console.ReadLine());
+
+Contract myContract = new Contract(contractNumber, contractDate, contractValue);
+
+ContractService contractService = new ContractService(new PayPalService());
+contractService.ProcessContract(myContract, months);
+
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.WriteLine("Installments: ");
+Console.ResetColor();
+
+foreach (Installment installment in myContract.Installments)
+{
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.WriteLine(installment);
+    Console.ResetColor();
+}
+
+
+
---
+++ IComparable/Program.cs
@@ -0,0 +1,28 @@
+﻿using Interface_IComparable.Entities;
+
+string path = @"C:\Users\elisilu\Documents\C#\func.txt";
+
+try
+{
+    using (StreamReader sr = File.OpenText(path)) //Abrir e ler o arquivo
+    {
+        List<Employee> list = new List<Employee>();
+        while (!sr.EndOfStream)
+        {
+            list.Add(new Employee(sr.ReadLine()));
+        }
+        list.Sort(); //list.Sort(); Ordenar uma lista
+        foreach (Employee emp in list)
+        {
+            Console.WriteLine(emp);
+        }
+
+
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine("An error ocurred: ");
+    Console.WriteLine(e.Message);
+}
+
